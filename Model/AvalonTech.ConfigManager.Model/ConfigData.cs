ae4e556e9b308d7a0ec87bdf439f43efb70aa9b7
DTVPAN\cgonzal7 <cgonzal7@directvla.com.ar>
2022-03-20 04:56:37 -0300

    Initial sync

---
+++ Model/AvalonTech.ConfigManager.Model/ConfigData.cs
@@ -0,0 +1,13 @@
+﻿namespace AvalonTech.ConfigManager.Model
+{
+    public class ConfigData: ITrackChanges
+    {
+        public string KeyName { get; set; }
+        public string Value { get; set; }
+        public string Description { get; set; }
+        public string UserCreated { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string UserModified { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}
